df6a22224b1ca79adcac9ec5544ca9ef005dafb4
Dania Tamim <dania.tamim@live.se>
2024-09-18 11:16:40 +0200

    Add project files.

---
+++ OOP-Basic/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basic
+{
+    internal class Person
+    {
+        public string Name { get; set; } = String.Empty;
+    }
+
+    internal class Employee : Person { 
+        public int salary { get; set; }
+
+    }
+
+    internal class Admin : Employee
+    {
+        public string department { get; set; } = String.Empty;
+    }
+}
---
+++ OOP-Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP_Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Name = "Dania";
+
+            Employee e = new Employee();
+            e.Name = "Kalle";
+            e.salary = 20000;
+
+            Admin a = new Admin();
+            a.Name = "Nisse";
+            a.salary = 30000;
+            a.department = "IT";
+
+        }
+    }
+}
