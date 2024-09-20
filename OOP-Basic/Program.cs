032e328d6ff29923ec880f35eb32fd1261259a64
Dania Tamim <dania.tamim@live.se>
2024-09-20 08:43:10 +0200

    Finish first part

--- OOP-Basic/Program.cs
+++ OOP-Basic/Program.cs
@@ -1,8 +1,15 @@
-﻿namespace OOP_Basic
+﻿
+namespace OOP_Basic
 {
     internal class Program
     {
         static void Main(string[] args)
+        {
+            FirstPart();
+
+        }
+
+        private static void FirstPart()
         {
             Person p = new Person();
             p.Name = "Dania";
@@ -16,6 +23,19 @@
             a.salary = 30000;
             a.department = "IT";
 
+            Employee admin = new Admin();
+            Person admin2 = new Admin();
+            Admin admin3 = new Admin();
+
+
+            Do(admin);
+            Do(admin2);
+            Do(admin3);
+        }
+
+        private static void Do(Person person)
+        {
+            Console.WriteLine(person);
         }
     }
 }
---
+++ OOP-Basic/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basic
+{
+    internal class Vehicle
+    {
+        public string Brand {  get; set; }
+
+        public Vehicle(string brand)
+        {
+            Brand = brand;
+        }
+    }
+}
